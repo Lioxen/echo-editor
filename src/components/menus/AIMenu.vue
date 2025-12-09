b53b22d3341e2033b4da190a726a2b79639bc507
LeeAt67 <1036232425@qq.com>
2025-12-09 21:13:53 +0800

    fix: 添加 "style="{       zIndex: status === 'init' && prompt === '' || status === 'completed' ? -1 : 98,     }"" 来修复当状态为completed时，不能够进行滚动的问题

--- src/components/menus/AIMenu.vue
+++ src/components/menus/AIMenu.vue
@@ -273,7 +273,7 @@ function handleKey(e) {
   <div
     class="absolute left-0 right-0 top-0 bottom-0"
     :style="{
-      zIndex: status === 'init' && prompt === '' ? -1 : 98,
+      zIndex: status === 'init' && prompt === '' || status === 'completed' ? -1 : 98,
     }"
     v-show="shouldShow"
     @click="handleOverlayClick"
